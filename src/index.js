bec143b283ebc1187439d33a574df6157fc3b5ac
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-09-21 14:46:44 -0400

    Corregir rutas e importaciones en index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import  express  from "express";
-import v1TareasRoutes  from "../routes/v1/tareas.routes.js";
-import v2TareasRoutes  from "../routes/v2/tareas.routes.js";
-import authRoutes  from "../routes/auth.routes.js";
-import apikeyMiddleware  from "../middlewares/apikey.middleware.js";
-import authMiddleware  from "../middlewares/auth.middleware.js";
+import "dotenv/config"
+import express from "express";
+import v1TareasRoutes from "./routes/v1/tareas.routes.js";
+import v2TareasRoutes from "./routes/v2/tareas.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import apikeyMiddleware from "./middlewares/apikey.middleware.js";
+import authMiddleware from "./middlewares/authmiddleware.js";
 
 const app = express();
 app.use(express.json());
@@ -12,21 +13,14 @@ const PORT = process.env.PORT || 3000;
 app.use("/auth", authRoutes);
 app.use("/v1/tareas", apikeyMiddleware, v1TareasRoutes);
 app.use("/v2/tareas", authMiddleware, v2TareasRoutes);
-app.use("/auth", authRoutes);
-
 
 const errorHandler = (err, req, res, next) => {
     console.error(err.message);
     res.status(500).json({ error: "Error interno del servidor" });
-
 }
 
 app.use(errorHandler);
 
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
