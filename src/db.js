2846edfc81433ada4a05ba76a52edc2d5cbbf40f
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-09-21 15:03:17 -0400

    correcion export en index.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
-import {PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
 });
 
-const prisma = new PrismaClient({
+export const prisma = new PrismaClient({
     adapter,
 });
